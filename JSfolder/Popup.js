aea8c2a8e40d58043936dbf88ca6c9dc63a5bdad
TAGSyuriken <72136842+TAGSyuriken@users.noreply.github.com>
2023-05-02 09:35:21 +0900

    update

--- JSfolder/Popup.js
+++ JSfolder/Popup.js
@@ -2,6 +2,19 @@ $('#R18').on('click', function(){
     if(!confirm('18歳以上ですか？')){
         return false;
     } else {
-        location.href = './HTMLfolder/R18.html'
+        var QUESTION_SENTENCE = ["2^2", "4^2", "2^10"];
+        var QUESTION_ANSWER = ["4", "16", "1024"];
+        var num = Math.floor(Math.random() * QUESTION_SENTENCE.length);
+        var User = window.prompt(`${QUESTION_SENTENCE[num]}の答えは？`);
+
+        if(User == QUESTION_ANSWER[num]){
+            alert('正解！');
+            setTimeout(function(){
+                location.href = './HTMLfolder/R18.html';
+            }, 5*1000);
+        } else {
+            console.log('不正解');
+        }        
+
     }
-})
+});
